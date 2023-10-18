4f71efd9caa0ac75f4eed99073beaea062935fce
msaio <nq2uit@gmail.com>
2023-10-18 09:03:40 +0700

    Inititate

---
+++ content.js
@@ -0,0 +1,30 @@
+function checkForElementAndHide() {
+  // const popupContainer = document.querySelector('ytd-popup-container.style-scope.ytd-app');
+  const classes = [
+    "ytd-popup-container.style-scope.ytd-app",
+    "tp-yt-iron-overlay-backdrop",
+    "tp-yt-iron-overlay-backdrop.opened",
+    "tp-yt-iron-overlay-backdrop[opened]",
+    "ytd-popup-container.style-scope ytd-app"
+  ]
+  for(i of classes){
+    var popupContainer = document.querySelector(i);
+    if (popupContainer) {
+      // The element with the specified class exists.
+      popupContainer.style.display = 'none'; // Hide the element.
+      clearInterval(intervalId); // Clear the interval.
+      return;
+    }
+  }
+  // var popupContainer = document.querySelector('tp-yt-iron-overlay-backdrop');
+  // // tp-yt-iron-overlay-backdrop.opened
+
+  // if (popupContainer) {
+  //   // The element with the specified class exists.
+  //   popupContainer.style.display = 'none'; // Hide the element.
+  //   clearInterval(intervalId); // Clear the interval.
+  // }
+}
+
+// Set an interval to check for the element every 1000 milliseconds (1 second).
+const intervalId = setInterval(checkForElementAndHide, 1000);
